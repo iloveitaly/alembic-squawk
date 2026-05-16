f182e7600b651e9fa1fb272011fd5a25dc77d8a8
Michael Bianco <mike@mikebian.co>
2026-05-16 13:22:13 -0600

    fix: resolve GHA linting failure and remove redundant test file

--- tests/test_regex.py
+++ tests/test_regex.py
@@ -3,7 +3,9 @@ from alembic_squawk.cli import SQUAWK_IGNORE_RE
 
 def test_squawk_ignore_re():
     # Valid formats
-    assert SQUAWK_IGNORE_RE.findall("# squawk-disable rule1") == ["squawk-disable rule1"]
+    assert SQUAWK_IGNORE_RE.findall("# squawk-disable rule1") == [
+        "squawk-disable rule1"
+    ]
     assert SQUAWK_IGNORE_RE.findall("#squawk-disable rule2") == ["squawk-disable rule2"]
     assert SQUAWK_IGNORE_RE.findall("# -- squawk-ignore-file rule3") == [
         "squawk-ignore-file rule3"
